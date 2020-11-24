4de7cf6a74e8fdc3498a52bb91f987469934eb3c
DaraghColl <daraghcoll@hotmail.com>
2020-11-24 17:59:32 +0000

    gridsome app setup

---
+++ src/layouts/Default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="layout">
+    <h1>Layout</h1>
+    <slot/>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style lang="scss">
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <h1>NXNW</h1>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
